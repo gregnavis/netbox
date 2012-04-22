a72e5832159c01156c3e8fa1e34f698bbf5e6cce
Grzegorz Niewisiewicz <grzegorz@niewisiewicz.pl>
2012-04-22 19:13:23 +0200

    Add nbdump - a network traffic dumping tool
    nbdump listens on all interfaces and prints hex dumps of packets to the
    standard output.

---
+++ Makefile
@@ -0,0 +1,14 @@
+CC?=gcc
+CFLAGS?=-Wall -Wextra -Werror -ansi
+
+TOOLS=nbdump
+
+.PHONY: all
+all: $(TOOLS)
+
+.PHONY: clean
+clean:
+	-rm $(TOOLS)
+
+nbdump: nbdump.c
+	$(CC) -o $@ $^
---
+++ nbdump.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <net/ethernet.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+// The default size is enought to hold a whole ethernet frame (< 1524 bytes)
+#ifndef PACKET_BUFFER_SIZE
+#define PACKET_BUFFER_SIZE 2048
+#endif
+
+static void print_packet(const unsigned char *pkt, size_t pktlen)
+{
+	while (pktlen--) {
+		printf("%02x", *pkt++);
+	}
+
+	putchar('\n');
+}
+
+int main(int argc, char *argv[])
+{
+	unsigned char pktbuf[PACKET_BUFFER_SIZE];
+	int sockfd, res = 0;
+
+	sockfd = socket(AF_PACKET, SOCK_RAW, htons(ETH_P_ALL));
+	if (-1 == sockfd) {
+        perror("socket");
+		goto error;
+	}
+
+	while (1) {
+        ssize_t pktlen = read(sockfd, pktbuf, sizeof(pktbuf));
+        if (pktlen < 0) {
+            perror("read");
+            continue;
+        }
+        if (pktlen > 0) {
+            print_packet(pktbuf, pktlen);
+        }
+	}
+
+    close(sockfd);
+
+out:
+	return res;
+
+error:
+	res = 1;
+	goto out;
+}
