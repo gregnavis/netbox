2856b92ddbcc56b0e2cbefe081d198f020d781f6
Grzegorz Niewisiewicz <grzegorz@niewisiewicz.pl>
2012-05-11 21:28:43 +0200

    Print protocol names in nbeth

--- nbeth.c
+++ nbeth.c
@@ -7,9 +7,38 @@
 #include "config.h"
 #include "common.h"
 
+struct namevalue {
+	char *name;
+	int value;
+};
+
+static struct namevalue proto_names[] = {
+	{ "ip4", 0x0800 },
+	{ "arp", 0x0806 },
+	{ "rarp", 0x8035 },
+	{ NULL, 0 },
+};
+
+static const char *proto_name(int proto)
+{
+	struct namevalue *p;
+	const char *proto_name = "unknown";
+
+	for (p = proto_names; p->name; p++) {
+		if (p->value == proto) {
+			proto_name = p->name;
+			break;
+		}
+	}
+
+	return proto_name;
+}
+
 void printpkt(void)
 {
 	struct ethhdr *eth = (struct ethhdr *) pkt;
+	int proto = htons(eth->h_proto);
+
 	printf("eth.dst %02x:%02x:%02x:%02x:%02x:%02x ",
 		eth->h_dest[0],
 		eth->h_dest[1],
@@ -24,7 +53,7 @@ void printpkt(void)
 		eth->h_source[3],
 		eth->h_source[4],
 		eth->h_source[5]);
-	printf("eth.proto %04x ", htons(eth->h_proto));
+	printf("eth.proto %04x %s ", proto, proto_name(proto));
 	dumppkt(sizeof(*eth));
 }
 
